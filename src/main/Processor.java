fb5d413520b7604528b5344531d1cb5b854d195a
Vincente Campisi <catchcaseio@gmail.com>
2018-06-10 22:51:10 +0200

    add user input, edit default arg and general refactoring

--- src/main/Processor.java
+++ src/main/Processor.java
@@ -1,18 +1,21 @@
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.RDF;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 
 public class Processor {
-    private int something = 0;
-    private int numSubjects = 0;
-    private int numNameSpaces = 0;
 
     public static void main(String[] args) {
-        if (args.length != 1) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.print("Please provide a valid RDF URL (e.g. http://dbpedia.org/data/Nintendo.rdf ): ");
+            new Processor().process(br.readLine());
+        } catch (Exception e) {
+            System.out.println("Invalid input; using default: http://dbpedia.org/data/Linz.rdf");
             new Processor().process("http://dbpedia.org/data/Linz.rdf");
-        } else {
-            new Processor().process(args[0]);
         }
     }
 
@@ -24,23 +27,19 @@ public class Processor {
         System.out.println("Graph size: " + model.getGraph().size());
 
 //    Calculate and print the total number of subjects
+        int numSubjects = 0;
         calculate(model.listSubjects(), numSubjects, "Number of subjects: ");
 
 //    Calculate and print the total number of namespaces in use
+        int numNameSpaces = 0;
         calculate(model.listNameSpaces(), numNameSpaces, "Number of namespaces: ");
 
-        calculateSomething(model);
-    }
-
-    private void calculateSomething(Model model) {
-        model.listStatements(new SimpleSelector(null, RDF.type, (RDFNode)null));
-        ResIterator iterator = model.listSubjectsWithProperty(model.getProperty("http://dbpedia.org/ontology/abstract"));
-//        ResIterator iterator = model.listSubjectsWithProperty(model.getProperty("http://dbpedia.org/ontology/abstract"));
-        while (iterator.hasNext()) {
-            Resource r = iterator.next();
-            something++;
-        }
-        System.out.println("Number of birthplace listings: " + something);
+//        Calculate and print the total number of statements with rdf.type predicate
+        int numTypePredicates = 0;
+        calculate(model.listStatements(
+                new SimpleSelector(null, RDF.type, (RDFNode)null)),
+                numTypePredicates,
+                "Number of statements with rdf:type predicate: ");
     }
 
 //    Counts the number of iterator items
@@ -51,5 +50,4 @@ public class Processor {
         }
         System.out.println(message + counter);
     }
-
 }
